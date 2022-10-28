8082aa42ba3ce38852b2a7ff8a6a29f8f5cca6a1
Fabio Casati <115072049+reliableai@users.noreply.github.com>
2022-10-28 14:55:53 +0200

    heroku hello again

--- server.py
+++ server.py
@@ -1,23 +1,29 @@
 import streamlit as st
+import chess
+
 
 st.title("ML for Everybody")
 
 st.write("This is a test")
 
-import numpy as np 
-import matplotlib.pyplot as plt 
-from matplotlib.colors import LogNorm 
-dx, dy = 0.015, 0.05 
-x = np.arange(-4.0, 4.0, dx) 
-y = np.arange(-4.0, 4.0, dy) 
-X, Y = np.meshgrid(x,y) 
-min_max = np.min(x), np.max(x), np.min(y), np.max(y) 
-res = np.add.outer(range(8), range(8))%2 
-plt.imshow(res, cmap="binary_r")
-plt.xticks([])
-plt.yticks([])
-plt.title("Chess Board Using Matplotlib Python") 
-plt.show()
+board = chess.Board()
+board
 
-fig, ax = plt.subplots()
-st.pyplot(fig)
+
+# import numpy as np 
+# import matplotlib.pyplot as plt 
+# from matplotlib.colors import LogNorm 
+# dx, dy = 0.015, 0.05 
+# x = np.arange(-4.0, 4.0, dx) 
+# y = np.arange(-4.0, 4.0, dy) 
+# X, Y = np.meshgrid(x,y) 
+# min_max = np.min(x), np.max(x), np.min(y), np.max(y) 
+# res = np.add.outer(range(8), range(8))%2 
+# plt.imshow(res, cmap="binary_r")
+# plt.xticks([])
+# plt.yticks([])
+# plt.title("Chess Board Using Matplotlib Python") 
+# plt.show()
+
+# fig, ax = plt.subplots()
+# st.pyplot(fig)
